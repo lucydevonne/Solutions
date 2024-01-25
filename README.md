6d38203e496cdfc4da2e327857ef361a5bbc70f0
lucydevonne <wambuilucie7@gmail.com>
2024-01-25 14:02:51 -0500

    Leetcode solution

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 2. https://gist.github.com/lucydevonne/ab54558030919e7342ef0be4f16d86d9
 3. https://gist.github.com/lucydevonne/4298f988060e80edc5b59a80dac432e5
 4. 1st Jan Challenge - https://leetcode.com/submissions/detail/1156337223/
+5. 2nd Jan Challenge - https://leetcode.com/submissions/detail/1156744116/
--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-def assign_cookies(g, s):
-    g.sort() #g is the greed factor or list of greed factors
-    s.sort() # s is the size of the cookie or list of sizes of cookies
-
-    content_children = 0
-    current_cookie_size_index = 0
-
-
-    for greed_factor in g:
-        while current_cookie_size_index < len(s) and s[current_cookie_size_index] < greed_factor:
-            current_cookie_size_index += 1
-
-        if current_cookie_size_index < len(s):
-            content_children += 1
-            current_cookie_size_index += 1
-
-    return content_children
-
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b
+print(sum)
 
 
